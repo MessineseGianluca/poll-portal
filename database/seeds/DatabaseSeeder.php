d510d3a5793fe8983a1df5e687971a77e4bcc703
MessineseGianluca <gianluca.messi1@gmail.com>
2016-07-19 19:41:41 +0200

    Make seed (poll and questions)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	//seed random poolls
+        DB::table('polls')->insert([
+            'title' => str_random(15),
+            'start_date' => '20160721',
+            'end_date' => '20170721',
+        ]);
+
+        DB::table('polls')->insert([
+            'title' => str_random(15),
+            'start_date' => '20160721',
+            'end_date' => '20170721',
+        ]);
+
+        DB::table('polls')->insert([
+            'title' => str_random(15),
+            'start_date' => '20160722',
+            'end_date' => '20170122',
+        ]);
+
+        DB::table('polls')->insert([
+            'title' => str_random(15),
+            'start_date' => '20150722',
+            'end_date' => '20160222',
+        ]);
+        
+        //seeds random questions
+        $charset = 'abc';
+        for($i = 1; $i < 5; $i++) {
+        	for($j = 0; $j < 10; $j++) {
+        		DB::table('questions')->insert([
+          	        'text' => str_random(20) . "?",
+          			'type' => substr(str_shuffle($charset), 0, 1),
+          			'poll_id' => $i,
+        		]);
+        	}
+        }
+
+        //seeds random options
+        
+
     }
 }
