8c13bd844b08ccb3ea25ea5c5556e55b90c0d5ee
MessineseGianluca <gianluca.messi1@gmail.com>
2016-07-20 00:45:21 +0200

    Make welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.default')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="masthead">
+    <span>
+      <h1> Welcome to Poll-Portal! </h1>
+      <a href='/home'>
+        <button type="button" class="btn btn-secondary">Join us!</button>
+      </a>
+    </span>
+  </div>
 </div>
 @endsection
