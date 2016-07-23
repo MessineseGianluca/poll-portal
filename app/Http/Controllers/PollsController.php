4006e2b297a81d91fe307418d935d014d6bca90b
MessineseGianluca <gianluca.messi1@gmail.com>
2016-07-23 14:22:41 +0200

    Implement control for opened and single questions

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -29,15 +29,17 @@ class PollsController extends Controller
         /* Check if the user has already done the poll */       
         $user = Auth::user();
         $join = DB::table('joins')
-                        ->where('poll_id', $poll_id)
-                        ->where('user_id', $user->id)
-                        ->count();
+                    ->where('poll_id', $poll_id)
+                    ->where('user_id', $user->id)
+                    ->count();
         if($join > 0) 
             return redirect('/home');
         
         /* get poll informations */
         $poll = DB::table('polls')
-                        ->where('id', $poll_id)->first();
+                    ->select('id', 'start_date', 'end_date', 'title')
+                    ->where('id', $poll_id)
+                    ->first();
         
         /* check if $id is wrong and then poll doesn't exist */ 
         if(!is_object ( $poll )) 
@@ -53,22 +55,24 @@ class PollsController extends Controller
         }
 
         $questions = DB::table('questions')
-                            ->where('poll_id', $poll_id)
-                            ->get();
+                         ->select('id', 'text', 'type')
+                         ->where('poll_id', $poll_id)
+                         ->get();
 
         foreach ($questions as $key => $question) {
             $options = DB::table('options')
+                            ->select('id', 'text')
                             ->where('ques_id', $question->id)
                             ->get();
             $questions[$key]->options = $options;             
         }
 
         return view(
-                     'compile', 
-                     [
-                       'poll' => $poll,
-                       'questions' => $questions,
-                     ]
+            'compile', 
+            [
+                'poll' => $poll,
+                'questions' => $questions,
+            ]
         );
     }
     
@@ -92,10 +96,10 @@ class PollsController extends Controller
                 foreach ($answer as $option) {
                     DB::table('answers')
                         ->insert(
-                                array(
-                                    'ques_id' => $ques_id, 
-                                    'content' => $option
-                                )
+                            array(
+                                'ques_id' => $ques_id, 
+                                'content' => $option
+                            )
                         );
                 }   
             }
@@ -103,10 +107,10 @@ class PollsController extends Controller
             else {
                 DB::table('answers')
                     ->insert(
-                            array(
-                                'ques_id' => $ques_id, 
-                                'content' => $answer
-                            )
+                        array(
+                            'ques_id' => $ques_id, 
+                            'content' => $answer
+                        )
                     );
             }
         }
@@ -125,7 +129,88 @@ class PollsController extends Controller
         
     }
 
-    public function show_result(Request $request, $id) {
-        return view('show');
+    public function show_result(Request $request, $poll_id) {
+        
+        $poll = DB::table('polls')
+                   ->select('id', 'start_date', 'end_date', 'title')
+                   ->where('id', $poll_id)
+                   ->first();
+
+        /* check if $poll_id is wrong and then poll doesn't exist */ 
+        if(!is_object ( $poll )) 
+            return redirect('/home');           
+
+        /* check if the poll is still opened */
+        if($poll->end_date > date('Y-m-d h:i:sa')) 
+            return redirect('/home');
+        
+        
+        /* check if the poll is yet to come */
+        if($poll->start_date > date('Y-m-d h:i:sa')) 
+            return redirect('/home');
+        
+        /* number of joins*/
+        $joins = DB::table('joins')
+                     ->where('poll_id', $poll_id)
+                     ->count();
+        if($joins == 0) 
+            return redirect('home');
+        
+        $questions = DB::table('questions')
+                        ->select('id', 'text', 'type')
+                        ->where('poll_id', $poll_id)
+                        ->get();
+        
+        foreach ($questions as $key => $question) {
+            
+            $answers = DB::table('answers')
+                           ->select('id', 'content')
+                           ->where('ques_id', $question->id)
+                           ->get();
+
+            /* opened questions */
+            if($question->type == 'a') {                           
+                //link answers to the relative question 
+                $questions[$key]->answers = $answers;
+            }
+            
+            /* single answer question */
+            else if($question->type == 'b') {
+
+                $options = DB::table('options')
+                               ->select('id', 'text')
+                               ->where('ques_id', $question->id)
+                               ->get();
+                
+                $questions[$key]->options = $options;
+                
+                $tot_num_of_answers = DB::table('answers')
+                                          ->where('ques_id', $question->id)
+                                          ->count();
+
+                foreach ($options as $o_key => $option) {
+                    $num_of_answers = DB::table('answers')
+                                          ->where('content', $option->id)
+                                          ->count();
+                    /* Calculate the percentual */
+                    $percentual = $num_of_answers / $tot_num_of_answers * 100;
+                    $questions[$key]->options[$o_key]->percentual = $percentual;
+                }
+            }
+
+            print_r($questions[$key]);
+            echo "<br><br><br>";
+
+        }                
+        
+
+        return view(
+            'show',
+            [
+                'poll' => $poll,
+                'questions' => $questions,
+                'joins' => $joins,
+            ]
+        );
     }
 }
