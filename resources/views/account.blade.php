c504ea8c548f75951c579af78e712a6f197c5206
MessineseGianluca <gianluca.messi1@gmail.com>
2016-07-28 16:48:34 +0200

    fix account view

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -13,14 +13,20 @@
 	<div class="row">
 	  <div class="col-lg-6">
 	  	<h2><small>Change your email</small></h2>
-	  	<form>
+	  	<form action="{{ url('/account/email') }}" method="post">
+	  	  
 	  	  {{ csrf_field() }}
+
+	  	  <!-- form method spoofing -->
+	  	  {{ method_field('PUT') }}
+
 			  <div class="input-group input-group-lg">
   				<span class="input-group-addon" id="sizing-addon1">
   					<span class="glyphicon glyphicon-envelope" aria-hidden="true">
   					</span>
   				</span>
-  				<input type="text" 
+  				<input type="text"
+  				       name="old_email" 
   				       class="form-control" 
   				       placeholder="Your old email..." 
   				       aria-describedby="sizing-addon1">
@@ -30,7 +36,8 @@
   					<span class="glyphicon glyphicon-envelope" aria-hidden="true">
   					</span>
   				</span>
- 					<input type="text" 
+ 					<input type="text"
+ 					       name="new_email" 
  					       class="form-control" 
  					       placeholder="Your new email..." 
  					       aria-describedby="sizing-addon1">
@@ -43,14 +50,20 @@
 	  </div>
 	  <div class="col-lg-6">
 	    <h2><small>Change your password</small></h2>
-	  	<form>
+	  	<form action="{{ url('/account/password') }}" method="post">
+	  	  
 	  	  {{ csrf_field() }}
+
+	  	  <!-- form method spoofing -->
+	  	  {{ method_field('PUT') }}
+
 				<div class="input-group input-group-lg">
   				<span class="input-group-addon" id="sizing-addon1">
   					<span class="glyphicon glyphicon-lock" aria-hidden="true">
   					</span>
   				</span>
- 					<input type="password" 
+ 					<input type="password"
+ 					       name="old_password" 
  					       class="form-control" 
  					       placeholder="Your new email..." 
  					       aria-describedby="sizing-addon1">
@@ -61,6 +74,7 @@
   					</span>
   				</span>
  					<input type="password" 
+ 					 			 name="new_password"
  					       class="form-control" 
  					       placeholder="Your new password..." 
  					       aria-describedby="sizing-addon1">
@@ -70,7 +84,8 @@
   					<span class="glyphicon glyphicon-lock" aria-hidden="true">
   					</span>
   				</span>
- 					<input type="password" 
+ 					<input type="password"
+ 								 name="new_password_confirm" 
  					       class="form-control" 
  					       placeholder="Your new password again..." 
  					       aria-describedby="sizing-addon1">
